a3d43e045347387ce002ec53a54a8750f4697916
alex <alexsilva.viana2006@gmail.com>
2024-11-25 19:16:36 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -3,33 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inicio</title>
+    <title>Início</title>
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-     <link rel = "stylesheet" type= "text/css" href = "lala.css">
+
+     <link rel = "stylesheet" type= "text/css" href = "CSS/lala.css">
+     
+<div class = "centralizar">
+    <h1>Floricultura Thalex</h1>
+</div>
+
+<div class="barraLateral">
+    <ul>
+        <li><a href="index.php">Início</a></li>
+        <li><a href="gerenciamento/categorias.php">categorias</a></li>
+        <li><a href="administração/usuarios.php">Login</a></li>
+    </ul>
+</div>
+
+
+    
+
 </head>
 <body>
-
-    <header>
-        <img src="img/porquinhogab.png" alt="logo">
-         <nav class= "navbar navbar-expand-lg bg-body-tertiary">
-            <div class= "conmtainer-fluid">
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Produtos</a></li>
-                <li><a href="#">Fornecedores</a></li>
-                <li><a href="#">Categorias</a></li>
-        </nav>
-     </ul>
-</header>
-<main class ="container">
-    <h2>conteúdo</h2>
-    <footer>
-        <h2>Rodapé</h2>
-    </footer>
-</main>
-
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    
 </body>
 </html>
